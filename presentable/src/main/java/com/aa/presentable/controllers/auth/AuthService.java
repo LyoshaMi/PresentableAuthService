4d929b1f766ed4cbda5848f212a0454de870028f
LyoshaMi <leshami@vk.com>
2023-01-20 22:56:27 +0300

    Initial commit

---
+++ presentable/src/main/java/com/aa/presentable/controllers/auth/AuthService.java
@@ -0,0 +1,53 @@
+package com.aa.presentable.controllers.auth;
+
+import com.aa.presentable.config.jwt.JwtService;
+import com.aa.presentable.controllers.RegisterRequest;
+import com.aa.presentable.entity.Role;
+import com.aa.presentable.entity.User;
+import com.aa.presentable.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ presentable/src/main/java/com/aa/presentable/entity/Role.java
@@ -0,0 +1,6 @@
+package com.aa.presentable.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
